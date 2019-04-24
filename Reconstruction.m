22847d037dd6b8d355e0250b68a3562da6d2c7c0
赵增君 <32793431+zhaozengj@users.noreply.github.com>
2019-04-24 19:57:57 +0800

    Update Reconstruction.m

--- Reconstruction.m
+++ Reconstruction.m
@@ -1,8 +1,8 @@
 function [T,F]=Reconstruction(I,p)
-%ÀûÓÃZernike¾ØÖØ½¨Í¼Ïñ
-%F:ÖØ½¨½á¹û
-%I:ÊäÈë¶şÖµÍ¼Ïñ
-%m:Zernike¾ØµÄ½×Êı
+%åˆ©ç”¨ZernikeçŸ©é‡å»ºå›¾åƒ
+%F:é‡å»ºç»“æœ
+%I:è¾“å…¥äºŒå€¼å›¾åƒ
+%m:ZernikeçŸ©çš„é˜¶æ•°
 load('Z1.mat');
 load('Z2.mat');
 Z1 = Z1';
@@ -15,37 +15,24 @@ y = x;
 [X,Y] = meshgrid(x,y);
 Theta = atan2((N-1-2.*Y+2),(2.*X-N+1-2));
 
-%%%%%     CPU     %%%%%
-% for n=0:p
-%    for m=-n:2:n
-%    %for m = n:-2:0
-%     [Znm,Rad]=Zernikmoment(I,n,m);
-%     Exp=exp(1i*m*Theta);
-%     F=F+Znm.*Rad.*Exp;
-%     %F=F+Znm.*Rad.*Exp;
-%     end
-% end
 
-%%%%%     CPU     %%%%%
-global Zjun1;
-global Zjun2;
-Zjun1 = zeros(50);
-Zjun2 = zeros(50);
+
+%%%%%     PU     %%%%%
+
 for n=0:p
     for m=-n:2:n
         if (m<0)
             k = -m;
             [Znm,Rad]=Zernikmoment(I,n,m);
             Exp=exp(1i*m*Theta);
-            Zjun2(n+1,k+1) = Znm;
+   
             F=F+Z2(n+1,k+1).*Rad.*Exp;
-            T=T+Znm.*Rad.*Exp;
         else
             [Znm,Rad]=Zernikmoment(I,n,m);
             Exp=exp(1i*m*Theta);
-            Zjun1(n+1,m+1) = Znm;
+ 
             F=F+Z1(n+1,m+1).*Rad.*Exp;
-            T=T+Znm.*Rad.*Exp;
+
         end
         %F=F+Znm.*Rad.*Exp;
     end
